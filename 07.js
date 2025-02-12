86e66afe1fac86a0a7ffaa3836543dcc12db373d
Antonio Matheus <matheus69969@gmail.com>
2025-02-12 15:06:15 -0300

    Adicionando arquivos JS

--- 07.js
+++ 07.js
@@ -1,5 +1,9 @@
 import convert from "convert"
 
+function BytesBits(Bytes) {
+    return convert(Bytes, 'B').to('bits');
+}
+console.log(`1024 Bytes é igual a: ${BytesBits(1024)} bits`);
 
 function DiasMinutos(dias){
 return convert(dias, 'days').to('minutes')
